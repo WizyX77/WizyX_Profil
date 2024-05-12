0f40a3fc46161b8fc3e041ddf3fb16e2deea9e5e
WizyX <156693069+WizyX77@users.noreply.github.com>
2024-05-12 21:07:35 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,12 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'WizyX',
-  description: 'Hello, I am @wizyx, I have been on the path of becoming a developer for 2 years. I live in Yalova, Türkiye.',
+  title: 'S2G Wizyx',
+  description: 'Hello, I am @_wizyx, I have been on the path of becoming a developer for 3 years. I live in Yalova, Türkiye.',
   themeColor: '#8B5CF6',
   robots: 'index, follow',
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
-  icons: 'https://cdn.discordapp.com/attachments/1225439296975015936/1231187910946984006/image.png?ex=66360c18&is=66239718&hm=5211c11f8f9eb9e084ddab52dfa8ae6c5859c5b49e361c536f9fd99c9f7bd329&',
+  icons: 'https://cdn.discordapp.com/attachments/972574061320368208/1239277762628288592/DcProfil.gif?ex=664256db&is=6641055b&hm=b69a720706971c18cd394970dfceba6521cd7fc4351406a5cdc5fb9f9e51e866&',
   openGraph: {
     images: 'https://share.creavite.co/6623972a9368fc5de79329e5.gif',
   },
